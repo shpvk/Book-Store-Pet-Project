f8d6ffcbdec4453015b0cf58afbd6f66106703b7
shpvk <holotovegor99@gmail.com>
2026-05-07 19:54:14 +0300

    Configure BookContext for EF Core

--- backend/BookStore.Infrastructure/Data/BookContext.cs
+++ backend/BookStore.Infrastructure/Data/BookContext.cs
@@ -1,10 +1,43 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BookStore.Infrastructure.Data
 {
-    public class BookContext : DbContext
+    public class BookContext(DbContextOptions<BookContext> options) : DbContext(options)
     {
+        public DbSet<Book> Books { get; set; }
 
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
     }
+
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(Book.MAX_TITLE_LENGTH);
+
+            builder.Property(x => x.Description)
+                .IsRequired()
+                .HasMaxLength(Book.MAX_DESCRIPTION_LENGTH);
+
+            builder.Property(x => x.Price)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(x => x.Image)
+                .IsRequired();
+
+        }
+    }
+
 }
